1da1267f468ba388ad5bbaeb874d5332b31ec525
Zadazou <zouw819@newschool.edu>
2024-11-13 15:38:56 -0500

    wed nov 13

---
+++ funstuff.js
@@ -0,0 +1,7 @@
+console.log("file is working!! :)")
+
+let button = document.getElementsById("button");
+
+button.addEventListener('click', function(event){
+    console.log("button clicked!!!!!!!!!!")
+})
